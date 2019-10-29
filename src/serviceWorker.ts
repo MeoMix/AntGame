34f1ffda484b4d4b75dfc7ce27b9073164478e45
Sean Anderson <Sean.Palmer.Anderson@gmail.com>
2019-10-28 17:26:06 -0700

    Fix default code; up and running.

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -20,7 +20,12 @@ const isLocalhost = Boolean(
     )
 );
 
-export function register(config) {
+type Config = {
+  onUpdate?: (registration: ServiceWorkerRegistration) => unknown,
+  onSuccess?: (registration: ServiceWorkerRegistration) => unknown,
+}
+
+export function register(config: Config) {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
@@ -54,7 +59,7 @@ export function register(config) {
   }
 }
 
-function registerValidSW(swUrl, config) {
+function registerValidSW(swUrl: string, config: Config) {
   navigator.serviceWorker
     .register(swUrl)
     .then(registration => {
@@ -98,7 +103,7 @@ function registerValidSW(swUrl, config) {
     });
 }
 
-function checkValidServiceWorker(swUrl, config) {
+function checkValidServiceWorker(swUrl: string, config: Config) {
   // Check if the service worker can be found. If it can't reload the page.
   fetch(swUrl)
     .then(response => {
